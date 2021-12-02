441fc62461493640b541333ee53c65127dd013d3
Alexey Boklogov <kashira87@mail.ru>
2021-12-02 23:57:32 +0300

    добавляет стили компоненту Balance

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -6,8 +6,8 @@ const Balance = () => {
   const totalBalance = useSelector(financeSelectors.totalBalance);
   return (
     <div className={s.balanceBox}>
-      <p>ваш баланс</p>
-      <p>{totalBalance}</p>
+      <p className={s.balanceText}>ваш баланс</p>
+      <p className={s.balanceNum}>₴ 24 000.00</p>
     </div>
   );
 };
