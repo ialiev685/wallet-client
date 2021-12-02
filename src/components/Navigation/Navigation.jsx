29c8fdbf217afb5e85f5478d93d9ae19d2fb3cff
JuliaZaykaS <sys55@mail.ru>
2021-12-02 15:00:19 +0300

    render currency & fill icons

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,3 +1,4 @@
+import Media from 'react-media';
 import { NavLink } from 'react-router-dom';
 import Icons from '../Icons';
 import s from './Navigation.module.css';
@@ -8,23 +9,23 @@ export const Navigation = () => {
 
   return (
     <nav className={s.navigation}>
-          <NavLink
-              to="/home"
-              className={setActiveClass}>
+      <NavLink to="/home" className={setActiveClass}>
         <Icons name={'home'} className={s.icon} />
         <span className={s.routeName}>Главная</span>
       </NavLink>
-          <NavLink
-              to="/diagram"
-              className={setActiveClass}>
+      <NavLink to="/diagram" className={setActiveClass}>
         <Icons name={'diagramm'} className={s.icon} />
         <span className={s.routeName}>Статистика</span>
       </NavLink>
-          <NavLink
-              to="/currency"
-              className={`${setActiveClass} ${s.currency}`}>
-              <Icons name={'currency'} className={s.icon} />
-      </NavLink>
+
+      <Media
+        query="(max-width: 767px)"
+        render={() => (
+          <NavLink to="/currency" className={setActiveClass}>
+            <Icons name={'currency'} className={s.icon} />
+          </NavLink>
+        )}
+      />
     </nav>
   );
 };
