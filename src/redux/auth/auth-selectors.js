c112da1daa90632a780e7820ac66ec615e5c01d0
Gulnaz Sydykbek <75367433+Gulnaz-Sydykbek@users.noreply.github.com>
2021-12-02 13:14:27 +0600

    ProtectedRoute

--- src/redux/auth/auth-selectors.js
+++ src/redux/auth/auth-selectors.js
@@ -1,1 +1,2 @@
-export const getisFetchingCurrent = (state) => state.auth.isFetchingCurrentUser;
+export const getIsLoggedIn = state => state.auth.isLoggedIn;
+export const getisFetchingCurrent = state => state.auth.isFetchingCurrentUser;
