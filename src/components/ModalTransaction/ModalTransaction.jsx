21ae473408b028831b9cd279e7d43d245f047668
Ilfat Aliev <ialiev685@gmail.com>
2021-12-02 12:31:17 +0300

    inputs настроил

--- src/components/ModalTransaction/ModalTransaction.jsx
+++ src/components/ModalTransaction/ModalTransaction.jsx
@@ -4,12 +4,13 @@ import { useFormik } from 'formik';
 import * as Yup from 'yup';
 import Datetime from 'react-datetime';
 import 'react-datetime/css/react-datetime.css';
+import NumberFormat from 'react-number-format';
 
 const validation = Yup.object({
   type: Yup.boolean(),
   amount: Yup.number()
     .min(0.01, 'Minimum amount 0.01')
-    .max(999999, 'Maximum amount 999999')
+    .max(999999999, 'Maximum amount 999999')
     .required('Required'),
   date: Yup.date().required(),
   discription: Yup.mixed(),
@@ -28,10 +29,15 @@ export const ModalTransaction = () => {
     validationSchema: validation,
 
     onSubmit: values => {
-      //   console.log(values);
+      console.log(values);
       alert(JSON.stringify(values, null, 2));
     },
   });
+
+  let inputDateProps = {
+    id: 'date',
+    name: 'date',
+  };
   return (
     <div>
       <div>
@@ -43,12 +49,13 @@ export const ModalTransaction = () => {
             id="type_pay"
             name="type_pay"
             type="checkbox"
+            checked={formik.values.type_pay}
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
-            value={formik.values.firstName}
+            value={formik.values.type_pay}
           />
           <label htmlFor="">Расход</label>
-
+          {/* 
           <input
             id="amount"
             name="amount"
@@ -56,16 +63,40 @@ export const ModalTransaction = () => {
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
             value={formik.values.amount}
+          /> */}
+
+          <NumberFormat
+            id="amount"
+            thousandSeparator={true}
+            format="### ### ###"
+            autoComplete="off"
+            // decimalSeparator="."
+            displayType="input"
+            type="text"
+            value={formik.values.amount}
+            onValueChange={(values, sourceInfo) => {
+              const { event } = sourceInfo;
+              formik.handleChange(event);
+            }}
+            thousandsGroupStyle="thousand"
           />
           {formik.touched.amount && formik.errors.amount ? (
             <div>{formik.errors.amount}</div>
           ) : null}
 
           {/* <label htmlFor="lastName">Last Name</label> */}
-          <Datetime id="date" name="date" value={formik.values.date} />
+          <Datetime
+            dateFormat="DD.MM.YYYY"
+            timeFormat={false}
+            inputProps={inputDateProps}
+            initialValue={formik.values.date}
+            closeOnSelect={true}
+          />
+          {/*id="date" name="date" value={formik.values.date}*/}
           <input
             id="discription"
             name="discription"
+            autoComplete="off"
             type="text"
             onChange={formik.handleChange}
             onBlur={formik.handleBlur}
