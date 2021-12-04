2ac0365d91387be8d9d34a2e2b0f76abcebac64c
LarisaZu <zueval@icloud.com>
2021-12-04 18:02:42 +0300

    add currency

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { authOperations, authSelectors } from "./redux/auth";
+import Currency from './components/Currency/Currency.jsx';
 
 function App() {
   const isFetchingCurrentUser = useSelector(authSelectors.getisFetchingCurrent);
@@ -19,6 +20,7 @@ function App() {
     !isFetchingCurrentUser && (
       <div className="App">
         <h1>Wallet</h1>
+        <Currency />
       </div>
     )
 
