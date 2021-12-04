25213b64107ec6a50492fc58b90a808dfb3c55fa
Alexey Boklogov <kashira87@mail.ru>
2021-12-04 19:09:26 +0300

    закончил со страницей регистрации

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -2,14 +2,14 @@ import s from './RegistrationPage.module.css';
 
 const RegistrationPage = () => {
   return (
-    <div className={s.registrBox}>
-      <div className={s.registrSubBox}></div>
-      <div className={s.registrEllipseOne}></div>
-      <div className={s.registrEllipseTwo}></div>
+    <div className={s.box}>
+      <h1 className={s.title}>Finance App</h1>
+      <div className={s.subBox}></div>
+      <div className={s.ellipseOne}></div>
+      <div className={s.ellipseTwo}></div>
       <div className={s.pictureTablet}></div>
       <div className={s.pictureDesktop}></div>
 
-      <div className={s.registrationForm}></div>
       {/* сюда вставить компонент 'RegistrationForm' с классом 'registrationForm' */}
     </div>
   );
