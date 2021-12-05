0002400ad0eb5dfc6627c135e108db64f4f80065
JuliaZaykaS <sys55@mail.ru>
2021-12-05 15:48:44 +0300

    container,section,fix nav styles

--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -2,14 +2,20 @@ import Navigation from 'components/Navigation';
 import Balance from 'components/Balance';
 import Currency from 'components/Currency/Сurrency';
 import ButtonAddTransactions from 'components/ButtonAddTransactions';
+import Container from 'components/Container';
+import Section from 'components/Section';
 
 export const HomeTab = () => {
   return (
-    <div>
-      <Navigation />
-      <Balance />
-      <Currency />
-      <ButtonAddTransactions />
-    </div>
+    <>
+      <Section>
+        <Container>
+          <Navigation />
+          <Balance />
+          <Currency />
+          <ButtonAddTransactions />
+        </Container>
+      </Section>
+    </>
   );
 };
