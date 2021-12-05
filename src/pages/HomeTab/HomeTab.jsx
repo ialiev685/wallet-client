076b9ef4ffc42ff6e2832c6bb2bf784c9fc04e62
JuliaZaykaS <sys55@mail.ru>
2021-12-05 22:45:02 +0300

    location on home page

--- src/pages/HomeTab/HomeTab.jsx
+++ src/pages/HomeTab/HomeTab.jsx
@@ -1,18 +1,31 @@
+import { useMediaQuery } from 'react-responsive';
 import Navigation from 'components/Navigation';
 import Balance from 'components/Balance';
 import Currency from 'components/Currency/Сurrency';
 import ButtonAddTransactions from 'components/ButtonAddTransactions';
 import Container from 'components/Container';
 import Section from 'components/Section';
+import s from './HomeTab.module.css';
 
 export const HomeTab = () => {
+  const isMobile = useMediaQuery({
+    query: '(max-width: 767px)',
+  });
   return (
     <>
       <Section>
         <Container>
-          <Navigation />
-          <Balance />
-          <Currency />
+          <div className={s.hometab}>
+            <div className={s.leftSideBox}>
+              <div>
+                <Navigation className={s.navigation} />
+                <Balance className={s.balance} />
+              </div>
+              {!isMobile && <Currency />}
+            </div>
+            <div className={s.table}>Table</div>
+          </div>
+
           <ButtonAddTransactions />
         </Container>
       </Section>
