afcadb6a90770090bf913a356c67cab6106879d1
Alexey Boklogov <kashira87@mail.ru>
2021-12-05 12:40:43 +0300

    убрал div-ы

--- src/pages/RegistrationPage/RegistrationPage.jsx
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -5,10 +5,6 @@ const RegistrationPage = () => {
     <div className={s.box}>
       <h1 className={s.title}>Finance App</h1>
       <div className={s.subBox}></div>
-      <div className={s.ellipseOne}></div>
-      <div className={s.ellipseTwo}></div>
-      <div className={s.pictureTablet}></div>
-      <div className={s.pictureDesktop}></div>
 
       {/* сюда вставить компонент 'RegistrationForm' с классом 'registrationForm' */}
     </div>
