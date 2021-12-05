7bad200104e536ad9230ffbb6443ca93048a2731
Alexey Boklogov <kashira87@mail.ru>
2021-12-06 01:06:02 +0300

    сделал страницу логина

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 // import './stylesheet/index.css'
 
-
 import { useEffect, useState } from 'react';
-
 
 import { ModalTransaction } from 'components/ModalTransaction';
 
 import Currency from './components/Currency/Сurrency.jsx';
 
-
 import { useDispatch, useSelector } from 'react-redux';
-
-// import RegistrationForm from 'components/RegistrationForm/RegistrationForm';
-import LoginForm from 'components/LoginForm/LoginForm';
 
 import { authOperations, authSelectors } from 'redux/auth';
 import Container from 'components/Container';
@@ -21,8 +15,8 @@ import Section from 'components/Section';
 import Balance from 'components/Balance';
 import Navigation from './components/Navigation';
 import RegistrationPage from './pages/RegistrationPage';
+import LoginPage from './pages/LoginPage';
 import Background from './pages/Background';
-
 
 function App() {
   const [showModal, setShowModal] = useState(false);
@@ -41,32 +35,27 @@ function App() {
   return (
     !isFetchingCurrentUser && (
       <div className="App">
-
-        {/* <RegistrationForm /> */}
-        {/* <LoginForm /> */}
         <Section>
           <Container>
-
             <Background>
               <RegistrationPage />
             </Background>
 
-            <LoginForm />
+            <Background>
+              <LoginPage />
+            </Background>
 
-               
-  <button onClick={() => setShowModal(prevShowModal => !prevShowModal)}>
-          Показать модалку
-        </button>
-        {/*тестовая кнопка, удалить после установки боевой*/}
-        {showModal && <ModalTransaction onClose={toggleModal} />}
-    
-       
+            <button
+              onClick={() => setShowModal(prevShowModal => !prevShowModal)}
+            >
+              Показать модалку
+            </button>
+            {/*тестовая кнопка, удалить после установки боевой*/}
+            {showModal && <ModalTransaction onClose={toggleModal} />}
 
             <Currency />
           </Container>
         </Section>
-
-        {/* <RegistrationForm /> */}
 
         {/* <Navigation /> */}
         {/* <Balance /> */}
