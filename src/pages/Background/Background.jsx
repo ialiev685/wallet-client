ce0ed98705d526246d22bfb726b359eaedce2ffa
JuliaZaykaS <sys55@mail.ru>
2021-12-12 15:58:15 +0300

    back start

--- src/pages/Background/Background.jsx
+++ src/pages/Background/Background.jsx
@@ -3,7 +3,11 @@ import s from './Background.module.css';
 const Background = ({ children, className = '' }) => {
   return (
     // <div className={s.colorBox}>
-    <div className={`${s.box} ${className}`}>{children}</div>
+    <div className={`${s.box} ${className}`}>
+      {/* <div className={s.blueCircle}></div> */}
+
+      {children}
+    </div>
     // </div>
   );
 };
